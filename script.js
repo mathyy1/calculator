784dbc7b8c4ea2ed4e173bad12d74b98657b9c58
matheus <csfyre2403@gmail.com>
2026-04-19 23:45:10 -0600

    Styled calculator with CSS

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+let num1;
+let operator;
+let num2;
+
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case '+':
+            return add(num1 + num2);
+        case '-':
+            return subtract(num1 + num2);
+        case '*':
+            return multiply(num1 + num2);
+        case '/':
+            return divide(num1 + num2);
+    }
+}
